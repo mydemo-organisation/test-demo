5dd16f25a0b944c90d4bfe3467ec1e4cd4dc4ded
harikesh <hari@gmail.com>
2023-10-21 17:45:32 +0000

    changed

--- mig.sh
+++ mig.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 for t in `git branch -a | grep 'tags/' | sed s_remotes/origin/tags/__` ; do
  git tag $t origin/tags/$t
- #git branch -d -r origin/tags/$t
+ git branch -d -r origin/tags/$t
 done
